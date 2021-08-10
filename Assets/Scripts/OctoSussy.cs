32e25f4c2f9d2f9f34f3599e3d454d04a127b5ca
GreenBoyWonder <grimlingai@gmail.com>
2021-08-09 23:52:40 -0700

    Initial Commit
    Pushing Up Current Changes

--- Assets/Scripts/OctoSussy.cs
+++ Assets/Scripts/OctoSussy.cs
@@ -4,18 +4,86 @@ using UnityEngine;
 
 public class OctoSussy : MonoBehaviour
 {
-
     public int RaysToShoot, maxDistance;
+    public float moveSpeed;
+
+    private Rigidbody2D rb;
     // Start is called before the first frame update
     void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+        StartCoroutine(PickState());
+    }
+
+    // Update is called once per frame
+   
+    void Update()
     {
         
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        Casts();
+        //Casts();
+    }
+
+    void Patrol()
+    {
+        int patrolTime = Random.Range(1, 3);
+
+        switch (patrolTime)
+        {
+            case(1):
+                rb.velocity = new Vector3(moveSpeed * Time.deltaTime, rb.velocity.y);
+                break;
+
+            case (2):
+                rb.velocity = new Vector3(-moveSpeed * Time.deltaTime, rb.velocity.y);
+                break;
+ 
+            case (3):
+                rb.velocity = Vector2.zero;
+                break;
+        }
+
+        StartCoroutine(PickState());
+    }
+
+    void MoveTowardPlayer()
+    {
+        //Check Distance by finding player
+
+    }
+
+    void Attack()
+    {
+        //Check Room Trigger to see if player is in room
+
+        //Check Distance by Trigger Radius
+        int attackSlot = Random.Range(1,2);
+
+    }
+
+    IEnumerator PickState()
+    {
+        int wait = Random.Range(1, 3);
+        yield return new WaitForSeconds(wait);
+        
+        float stateSlot = Random.Range(1, 2);
+        switch(stateSlot)
+        {
+            case (1):
+                Patrol();
+                Debug.Log("PAtrol");
+                break;
+            case (2):
+                Debug.Log("something else");
+                StartCoroutine(PickState());
+                break;           
+        }
+        
+
     }
 
     void Casts()
